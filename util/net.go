ad3aef28f9bfee822b99abb454463d4e0a0cb958
psi <ryo.hirafuji@link-u.co.jp>
2020-07-02 21:38:43 +0900

    Use or, not just assign.

--- util/net.go
+++ util/net.go
@@ -29,10 +29,10 @@ func GenerateIPAddr(v4Net *net.IPNet, v6Net *net.IPNet, id uint32) (net.IP, net.
 	v6 := cloneIP(v6Net.IP)
 	for left, pos4, pos6 := id, len(v4)-1, len(v6)-2; left != 0; left, pos4, pos6 = left >> 8, pos4 - 1, pos6 -2 {
 		decimalId := byte(left & 0xff)
-		v4[pos4] = decimalId
+		v4[pos4] |= decimalId
 		hexId := uint16(decimalId % 10) + uint16((decimalId / 10) % 10) * 16 + uint16(decimalId / 100) * 256
-		v6[pos6+0] = byte((hexId >> 8) & 0xff)
-		v6[pos6+1] = byte(hexId & 0xff)
+		v6[pos6+0] |= byte((hexId >> 8) & 0xff)
+		v6[pos6+1] |= byte(hexId & 0xff)
 	}
 	if !v4Net.Contains(v4) || v4.Equal(net.IPv4(0xff, 0xff, 0xff, 0xff).Mask(v4Net.Mask)) {
 		return nil, nil, fmt.Errorf("num of devices exceeds the limit of IP addr pool: ")
