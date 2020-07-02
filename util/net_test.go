c3b785d37ffa1cada5ca7c62d39b8e483e9acd8e
psi <ryo.hirafuji@link-u.co.jp>
2020-07-02 20:53:40 +0900

    Use private addrs

--- util/net_test.go
+++ util/net_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestCalcDefaultGatewayV6(t *testing.T) {
 	{
-		cidr := "1234:1234:1234:1234::/64"
-		gw := "1234:1234:1234:1234::1"
+		cidr := "fc00:1234:1234:1234::/64"
+		gw := "fc00:1234:1234:1234::1"
 		ip, err := CalcDefaultGateway(cidr)
 		if err != nil {
 			t.Error(err)
@@ -18,7 +18,7 @@ func TestCalcDefaultGatewayV6(t *testing.T) {
 		}
 	}
 	{
-		cidr := "1234:1234:1234:1234::/128"
+		cidr := "fc00:1234:1234:1234::/128"
 		ip, err := CalcDefaultGateway(cidr)
 		if err == nil {
 			t.Errorf("There should not be default GW for %s, but got %s", cidr, ip.String())
@@ -28,8 +28,8 @@ func TestCalcDefaultGatewayV6(t *testing.T) {
 
 func TestCalcDefaultGatewayV4(t *testing.T) {
 	{
-		cidr := "192.168.128.0/18"
-		gw := "192.168.128.1"
+		cidr := "127.168.128.0/18"
+		gw := "127.168.128.1"
 		ip, err := CalcDefaultGateway(cidr)
 		if err != nil {
 			t.Error(err)
@@ -39,7 +39,7 @@ func TestCalcDefaultGatewayV4(t *testing.T) {
 		}
 	}
 	{
-		cidr := "192.168.128.0/32"
+		cidr := "127.168.128.0/32"
 		ip, err := CalcDefaultGateway(cidr)
 		if err == nil {
 			t.Errorf("There should not be default GW for %s, but got %s", cidr, ip.String())
@@ -48,17 +48,17 @@ func TestCalcDefaultGatewayV4(t *testing.T) {
 }
 
 func TestGenerateIPAddr(t *testing.T) {
-	_, v4Net, _ := net.ParseCIDR("10.10.0.0/16")
-	_, v6Net, _ := net.ParseCIDR("fd80::/112")
+	_, v4Net, _ := net.ParseCIDR("127.10.0.0/16")
+	_, v6Net, _ := net.ParseCIDR("fc80::/112")
 	{
 		ipv4, ipv6, err := GenerateIPAddr(v4Net, v6Net, 100)
 		if err != nil {
 			t.Error("Failed to generate IP: ", err)
 		}
-		if expected := "10.10.0.100"; ipv4.String() != expected {
+		if expected := "127.10.0.100"; ipv4.String() != expected {
 			t.Errorf("Failed to generate IPv4: %s(expected) != %s(actual)", ipv4.String(), expected)
 		}
-		if expected := "fd80::100"; ipv6.String() != expected {
+		if expected := "fc80::100"; ipv6.String() != expected {
 			t.Errorf("Failed to generate IPv6: %s(expected) != %s(actual)", ipv6.String(), expected)
 		}
 	}
